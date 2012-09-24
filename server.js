a38e77fb267ce705fc7a72fc96e1266b8340ede0
matan <matan@cloudaloe.org>
2012-09-24 15:30:56 +0200

    no comment

--- server.js
+++ server.js
@@ -80,68 +80,138 @@ function requestHandler(request, response) {
 server.listen(port, null, null, function(){ 
 	console.log('Server listening on' + ': '  + port);});
 
-function mysqlPush(statement)
+function mysqlPush(statement, queryVars)
 {
     mysqlVerifyConnection();
-    mysqlConnection.query(statement, function(err, result) {
+    mysqlConnection.query(statement, queryVars, function(err, result) {
         if (err)
         {
-            console.log('Error encountered executing DDL: \n', err);
+            console.log('Error encountered executing in mysql: \n', statement, err);
             return err;
         }
     });
 }
 
-function mysqlGet(statement)
+
+//
+// Utility function for running a query that should return a single result value
+// It still for now returns a key value pair and not a single value,
+// so extract the value of the key from its return
+//
+function mysqlGetSingleResult(statement, queryVars, doneCallBack)
+{
+    mysqlGet(statement, queryVars, function(result) {
+        debugger;
+        //console.log('mysqlGetSingleResult' + result, statement, queryVars);
+        if (result.length == 1)
+        {
+            if (Object.keys(result[0]).length == 1)
+                for (key in result[0])
+                {
+                    // it's javascript, need to skip the standard inherited object properties
+                    if (result[0].hasOwnProperty(key))
+                        doneCallBack(result[0][key]);
+                }
+            else
+                console.log('Error getting single value from mysql: query result was not a single value as expected. The result follows \n', result);
+        }
+        else
+            if (result.length == 0)
+            {
+                doneCallBack(null);
+            }
+            else
+            {
+                console.log('Error getting single value from mysql: query result was not a single value as expected. The result follows \n', result);
+                //doneCallBack(null);
+            }
+    });
+}
+
+function mysqlGet(statement, queryVars, doneCallback)
 {
     mysqlVerifyConnection();
 
-    mysqlConnection.query(statement, function(err, result) {
+    mysqlConnection.query(statement, queryVars, function(err, result) {
         if (err)
         {
-            console.log('Error encountered executing mysql query: \n', err);
-            return err;
+            console.log('Error encountered executing in  mysql: \n', statement, err);
         }
         else
         {
-            for (i=0; i<result.length; i++)
-            {
-                console.log(result[i]);
-            }
-            return result;
+            //for (i=0; i<result.length; i++)  {console.log(result[i]);}
+            //console.log(JSON.stringify(result));
+            //console.log('mysqlGet' + statement + queryVars + result);
+             doneCallback(result);
         }
     });
 }
 
-
 function mysqlInitDB()
 {
+    //
+    // Initialize the database - should not be run as part of the normal flow
+    // Run once, for dev environments
+    //
+
     mysqlVerifyConnection();
 
     //
-    // valuesTableID will be used as a table name, hence its length set according to
-    // http://stackoverflow.com/questions/6868302/maximum-length-of-a-table-name-in-mysql
+    // valuesTableName will be used as a table name, hence its length set according to
+    // http://stackoverflow.com/questions/6868302/maximum-length-of-a-table-name-in-mysql.
     //
-    var statement = 'create table master (apiKey BIGINT UNSIGNED UNIQUE, identifier VARCHAR(20), unit VARCHAR(100), valuesTableID VARCHAR(64))';
-     mysqlPush(statement);
+    // note that the mysql INT data type is being used, because node-mysql doesn't properly handle BIGINT at present, Sep 2012
+    // It doesn't convert them into numbers but strings, and also https://github.com/Sannis/node-mysql-libmysqlclient/issues/110
+    // may indicate BIGINT is not supported
+    //
+    var statement = 'create table master (apiKey INT UNSIGNED, ' +
+                                                                                            'identifier VARCHAR(20), ' +
+                                                                                            'unit VARCHAR(100), ' +
+                                                                                            'valuesTableNumPart INT UNSIGNED, ' +
+                                                                                            'valuesTableName VARCHAR(64))';
+    mysqlPush(statement);
+}
+
+function mysqlFindEntity(apiKey, identifiersArray)
+{
+    mysqlGetSingleResult('select valuesTableNumPart from master where apiKey = ?', apiKey, function(result){
+        if (!result)
+            console.log('Entity ' + apiKey + ' not defined in mysql database');
+        else
+            console.log('Entity ' + apiKey + ' found in mysql database, and has entity values table ' + result + ' associated to it');
+    });
 }
 
 function mysqlNewEntityInit(name)
 {
-    //
-    //  first, get a unique ID to use for the new table
-    // this ID determines the name of the table in which
-    // the datums of the entity will be stored
+    debugger;
+    mysqlGetSingleResult('select max(valuesTableNumPart) from master', null, function(entityValuesTableNumPart) {
+        //
+        //  first, get a unique ID to use for the new table
+        // this ID determines the name of the table in which
+        // the datums of the entity will be stored
+        //
+        console.log(entityValuesTableNumPart);
+        if (!entityValuesTableNumPart) // master table still empty
+            entityValuesTableNumPart= 1;
+         else
+            entityValuesTableNumPart += 1;
 
-    var valuesTableID = mysqlGet('select max(valuesTableID)  as max from master').max;
+        entityValuesTableName = 'ev' + entityValuesTableNumPart.toString();
+        console.log(entityValuesTableNumPart, ' ', entityValuesTableName);
 
+        // create a table for the metric values
+        mysqlPush('create table ' + entityValuesTableName + ' (timestamp TIMESTAMP, value float)');
 
-
-    // create entries in the master table
-
-    name += parseString()
-    // create a table for the metric values
-    mysqlPush('create table ' + name + ' (timestamp TIMESTAMP, value float)');
+        // create entries in the master table
+        mysqlPush('insert into master SET ?', {
+            apiKey: '9833',
+            identifier: 'server3',
+            unit:'percent',
+            valuesTableNumPart:entityValuesTableNumPart,
+            valuesTableName: entityValuesTableName
+        });
+    });
 }
 
 function mysqlVerifyConnection()
@@ -188,9 +258,11 @@ function stupidTestMysqlDB()
     mysqlConnection.end();
 }
 
-//mysqlInitDB();
-//mysqlNewEntityInit('metric69498');
+
+mysqlInitDB();
+//mysqlNewEntityInit(null);
 //stupidTestMysqlDB();
-var valuesTableID = mysqlGet('select max(valuesTableID)  as max from master');
-debugger;
+//mysqlGetSingleResult('select max(valuesTableNumPart) from master', function(result) {console.log(result);});
+//if (!mysqlFindEntity(234234349, null))
+   mysqlNewEntityInit(234234349);
 
